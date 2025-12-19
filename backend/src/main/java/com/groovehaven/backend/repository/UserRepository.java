2ebf23a5b4b82c0dbe10b027a68237375d2bbf9d
Thevindu Dharmadasa <thevinduhansaja81@gmail.com>
2025-12-19 14:20:33 +0530

    Added search method for finding Artists by username✅

--- backend/src/main/java/com/groovehaven/backend/repository/UserRepository.java
+++ backend/src/main/java/com/groovehaven/backend/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.groovehaven.backend.repository;
 
 import com.groovehaven.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // This magic method finds a user just by typing their name!
     Optional<User> findByUsername(String username);
+    List<User> findByRole(String role);
+
+    // 👇 NEW: Search for Artists by username (Case Insensitive)
+    List<User> findByRoleAndUsernameContainingIgnoreCase(String role, String username);
 }
